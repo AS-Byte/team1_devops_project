251aa184a5c7c631363d1f7a2d935cd76c59ba1d
Hanen-Azzouz <hanen-azzouz@hotmail.com>
2023-04-01 00:51:54 +0100

    pom.xml

--- src/test/java/tn/esp/team1/Team1ApplicationTests.java
+++ src/test/java/tn/esp/team1/Team1ApplicationTests.java
@@ -2,15 +2,19 @@ package tn.esp.team1;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 @SpringBootTest
 class Team1ApplicationTests {
 
     @Test
     void contextLoads() {
+
+
     }
 
 }
